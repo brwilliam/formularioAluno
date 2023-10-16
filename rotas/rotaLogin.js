9b9929c07a17934904aece36c80fddb2163cd6ba
brwilliam <brunowilliam997@gmail.com>
2023-10-16 18:23:51 -0300

    Alterado as rotas

--- rotas/rotaLogin.js
+++ rotas/rotaLogin.js
@@ -4,16 +4,16 @@ const rotaLogin = Router();
 
 rotaLogin
   .get("1", (requisicao, resposta) => {
-    resposta.redirect("/voltar.html");
+    resposta.redirect("/aluno6-ppiadsead/voltar.html");
   })
   .post("/", (requisicao, resposta) => {
     const usuario = requisicao.body.usuario;
     const senha = requisicao.body.senha;
     if (usuario === 'Renato' && senha ==='123') {
       requisicao.session.usuarioLogado = true;
-      resposta.redirect("/cadastroAluno.html");
+      resposta.redirect("/aluno6-ppiadsead/cadastroAluno.html");
     } else {
-      resposta.redirect("/voltar.html");
+      resposta.redirect("/aluno6-ppiadsead/voltar.html");
     }
   });
 
