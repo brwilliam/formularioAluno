4d6cdbb54c3bea7a2565929c01479876600e8e1a
brwilliam <brunowilliam997@gmail.com>
2023-10-23 19:33:00 -0300

    corrigindo erro tabela

--- protegido/scripts/scripts.js
+++ protegido/scripts/scripts.js
@@ -17,15 +17,16 @@ function obterAlunos(){
     });
 }
 
-function mostrarAlunos(listaAlunos){
+function mostrarAlunos(listaAlunos) {
     let elementoDivTabela = document.getElementById('espacoTabela');
-    if(listaAlunos.length > 0){
-    //limpar elemento div tabela 
-   
-        elementoDivTabela.innerHTML ='' ;
-        let tabela          = document.createElement('table');
+
+    if (listaAlunos.length > 0) {
+        // Limpar elemento div tabela 
+        elementoDivTabela.innerHTML = '';
+
+        let tabela = document.createElement('table');
         let cabecalhoTabela = document.createElement('thead');
-        let corpoTabela     = document.createElement('tbody');
+        let corpoTabela = document.createElement('tbody');
 
         cabecalhoTabela.innerHTML = `<tr>
                                         <th>nome</th>
@@ -37,30 +38,27 @@ function mostrarAlunos(listaAlunos){
                                         <th>email</th>
                                         <th>celular</th>
                                         <th>estadoCivil</th>
-                                    </th>`;
-        //adicionando cabecalho na tabela                          
+                                    </tr>`;
+
         tabela.appendChild(cabecalhoTabela);
-        //alimentar o corpo com a lista de clientes
-        for (const aluno of listaAlunos){
+
+        for (const aluno of listaAlunos) {
             const linhaTabela = document.createElement('tr');
-            linhaTabela.innerHTML = `<td>${aluno.nome}</td>`
-                                    `<td>${aluno.cpf}</td>`
-                                    `<td>${aluno.dataNasc}</td>`
-                                    `<td>${aluno.genero}</td>`
-                                    `<td>${aluno.endereco}</td>`
-                                    `<td>${aluno.bairro}</td>`
-                                    `<td>${aluno.email}</td>`
-                                    `<td>${aluno.celular}</td>`
-                                    `<td>${aluno.estadoCivil}</td>`;
+            linhaTabela.innerHTML = `<td>${aluno.nome}</td>
+                                    <td>${aluno.cpf}</td>
+                                    <td>${aluno.dataNasc}</td>
+                                    <td>${aluno.genero}</td>
+                                    <td>${aluno.endereco}</td>
+                                    <td>${aluno.bairro}</td>
+                                    <td>${aluno.email}</td>
+                                    <td>${aluno.celular}</td>
+                                    <td>${aluno.estadoCivil}</td>`;
             corpoTabela.appendChild(linhaTabela);
         }
+
         tabela.appendChild(corpoTabela);
         elementoDivTabela.appendChild(tabela);
+    } else {
+        elementoDivTabela.innerHTML = '<div class="alert alert-warning" role="alert">Nenhum aluno cadastrado!</div>';
     }
-    else{
-        elementoDivTabela.innerHTML = `<div class="alert alert-warning" role="alert">
-                                            Nenhum aluno cadastrado!
-                                       </div>`
-    }
-
 }
