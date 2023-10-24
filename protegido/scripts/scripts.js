b1374fd9bb471c6a5cef6e27f5132ccde6f3dc5f
brwilliam <brunowilliam997@gmail.com>
2023-10-23 22:16:00 -0300

    ajuste tabela

--- protegido/scripts/scripts.js
+++ protegido/scripts/scripts.js
@@ -25,7 +25,7 @@ function mostrarAlunos(listaAlunos) {
         elementoDivTabela.innerHTML = '';
 
         let tabela = document.createElement('table');
-        tabela.className = 'table table-dark ';
+        tabela.className = 'table table-dark';
         let cabecalhoTabela = document.createElement('thead');
         let corpoTabela = document.createElement('tbody');
 
